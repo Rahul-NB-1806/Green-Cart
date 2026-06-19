be2df4407d555a497ff653cecf3b374942020623
Rahulnb1086 <rahulnb1806@gamil.com>
2026-06-19 15:53:59 +0530

    css fix part 4 (package changes)

--- frontend/src/fixCss.js
+++ frontend/src/fixCss.js
@@ -1,3 +1,3 @@
 const fs = require('fs');
-const css = fs.readFileSync('src/index.compiled.css', 'utf8');
-fs.writeFileSync('src/index.compiled.css', css.replaceAll('calc(infinity * 1px)', '9999px'));
+const css = fs.readFileSync('public/index.compiled.css', 'utf8');
+fs.writeFileSync('public/index.compiled.css', css.replaceAll('calc(infinity * 1px)', '9999px'));
