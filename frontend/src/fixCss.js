327a37f958bec2d4ebe59c4c1c285d603d61a4a9
Rahulnb1086 <rahulnb1806@gamil.com>
2026-06-19 13:44:10 +0530

    css fix

--- frontend/src/fixCss.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const css = fs.readFileSync(path.join(__dirname, 'index.compiled.css'), 'utf8');
-fs.writeFileSync(path.join(__dirname, 'index.compiled.css'), css.replace(/calc\(infinity \* 1px\)/g, '9999px'));
